1e1d5d6525b51d67633a45cefb2a1f8620fe0792
Torres Ng <torresng2684cte@gmail.com>
2019-03-04 01:33:14 +0800

    Sort Numbers

--- Fundamentals/sort_numbers/src/lib.rs
+++ Fundamentals/sort_numbers/src/lib.rs
@@ -1,3 +1,5 @@
+extern crate rand;
+
 pub fn sort_numbers(arr: &Vec<i32>) -> Vec<i32> {
     let mut vec = arr.clone();
     vec.sort();
@@ -7,6 +9,14 @@ pub fn sort_numbers(arr: &Vec<i32>) -> Vec<i32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::Rng;
+
+    fn solution(arr: &Vec<i32>) -> Vec<i32> {
+        let mut clone = arr.clone();
+        clone.sort();
+
+        return clone;
+    }
 
     #[test]
     fn sample_tests() {
@@ -15,5 +25,17 @@ mod tests {
         assert_eq!(sort_numbers(&vec![20, 2, 10]), vec![2, 10, 20]);
         assert_eq!(sort_numbers(&vec![2, 20, 10]), vec![2, 10, 20]);
         assert_eq!(sort_numbers(&vec![2, 10, 20]), vec![2, 10, 20]);
+    }
+
+    #[test]
+    fn random_tests() {
+        for _i in 0..100 {
+            // Makes a random vector with length (rnd 0 to 100) with elements (rnd 0 to 100)
+            let arr: Vec<i32> = (0..rand::thread_rng().gen_range(0, 101))
+                .map(|_| rand::thread_rng().gen_range(0, 101))
+                .collect();
+
+            assert_eq!(sort_numbers(&arr), solution(&arr));
+        }
     }
 }
