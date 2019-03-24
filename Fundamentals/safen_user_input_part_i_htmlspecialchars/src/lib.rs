0afa62e4b19212ee9251c0d2f51bb74fbb4de65c
Torres Ng <torresng2684cte@gmail.com>
2019-03-24 15:25:05 +0800

    Safen User Input Part I - htmlspecialchars

--- Fundamentals/safen_user_input_part_i_htmlspecialchars/src/lib.rs
+++ Fundamentals/safen_user_input_part_i_htmlspecialchars/src/lib.rs
@@ -1,5 +1,14 @@
 pub fn html_special_chars(html: &str) -> String {
-    html.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;")
+    html.chars().fold(String::new(), |mut s, x| {
+        match x {
+            '<' => s.push_str("&lt;"),
+            '>' => s.push_str("&gt;"),
+            '"' => s.push_str("&quot;"),
+            '&' => s.push_str("&amp;"),
+            _ => s.push(x),
+        };
+        s
+    })
 }
 
 #[cfg(test)]
@@ -8,29 +17,37 @@ extern crate rand;
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     use rand;
     use rand::Rng;
-    
+
     fn solution(html: &str) -> String {
         html.replace("&", "&amp;")
             .replace("<", "&lt;")
             .replace(">", "&gt;")
             .replace("\"", "&quot;")
     }
-    
+
     #[test]
     fn sample_tests() {
-        assert_eq!(html_special_chars("<h2>Hello World</h2>"), 
-            "&lt;h2&gt;Hello World&lt;/h2&gt;");
-        assert_eq!(html_special_chars("Hello, how would you & I fare?"), 
-            "Hello, how would you &amp; I fare?");
-        assert_eq!(html_special_chars("How was \"The Matrix\"?  Did you like it?"), 
-            "How was &quot;The Matrix&quot;?  Did you like it?");
-        assert_eq!(html_special_chars("<script>alert('Website Hacked!');</script>"), 
-            "&lt;script&gt;alert('Website Hacked!');&lt;/script&gt;");
+        assert_eq!(
+            html_special_chars("<h2>Hello World</h2>"),
+            "&lt;h2&gt;Hello World&lt;/h2&gt;"
+        );
+        assert_eq!(
+            html_special_chars("Hello, how would you & I fare?"),
+            "Hello, how would you &amp; I fare?"
+        );
+        assert_eq!(
+            html_special_chars("How was \"The Matrix\"?  Did you like it?"),
+            "How was &quot;The Matrix&quot;?  Did you like it?"
+        );
+        assert_eq!(
+            html_special_chars("<script>alert('Website Hacked!');</script>"),
+            "&lt;script&gt;alert('Website Hacked!');&lt;/script&gt;"
+        );
     }
-    
+
     #[test]
     fn advanced_example() {
         let mut message = "<h2>Lorem Ipsum</h2>\n".to_string();
@@ -41,40 +58,41 @@ mod tests {
         message.push_str(&"// Some very malicious code here - a redirect!\n");
         message.push_str(&"window.location = \"http://www.hacked.com/\";\n");
         message.push_str(&"</script>");
-        
+
         // Coercion from struct String to str
-        let message : &str = &message;
-        
+        let message: &str = &message;
+
         let mut safened_message = "&lt;h2&gt;Lorem Ipsum&lt;/h2&gt;\n".to_string();
-        safened_message.push_str(&"&lt;p&gt;Lorem ipsum dolor sit amet, adispicing eu.&lt;/p&gt;\n");
-        safened_message.push_str(&"&lt;p&gt;Lorem ipsum dolor sit amet, adispicing eu.&lt;/p&gt;\n");
+        safened_message
+            .push_str(&"&lt;p&gt;Lorem ipsum dolor sit amet, adispicing eu.&lt;/p&gt;\n");
+        safened_message
+            .push_str(&"&lt;p&gt;Lorem ipsum dolor sit amet, adispicing eu.&lt;/p&gt;\n");
         safened_message.push_str(&"&lt;p&gt;Hello Website Owner,&lt;br /&gt; I think you &amp; I can make an awesome &quot;hacking pair&quot;.  What do you think?&lt;/p&gt;\n");
         safened_message.push_str(&"&lt;script&gt;\n");
         safened_message.push_str(&"// Some very malicious code here - a redirect!\n");
         safened_message.push_str(&"window.location = &quot;http://www.hacked.com/&quot;;\n");
         safened_message.push_str(&"&lt;/script&gt;");
-        
+
         // Coercion from struct String to str
-        let safened_message : &str = &safened_message;
+        let safened_message: &str = &safened_message;
         assert_eq!(html_special_chars(message), safened_message);
     }
-    
+
     #[test]
     fn random_test() {
-        let chars : Vec<char> = "&<>\"abcdefghijklmnopqrstuvwxyz".chars().collect();
-    
+        let chars: Vec<char> = "&<>\"abcdefghijklmnopqrstuvwxyz".chars().collect();
+
         for _ in 0..100 {
             let mut html = String::new();
             let length = rand::thread_rng().gen_range(1, 1001);
-            
+
             for _ in 0..length {
                 html.push(chars[rand::thread_rng().gen_range(0, chars.len())]);
             }
-            
-            let html : &str = &html;
-            
+
+            let html: &str = &html;
+
             assert_eq!(html_special_chars(html), solution(html));
         }
     }
 }
-
