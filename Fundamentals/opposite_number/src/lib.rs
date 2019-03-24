5fc45d08a10763ecc18023b90013429f06ee03c7
Torres Ng <torresng2684cte@gmail.com>
2019-03-24 15:34:08 +0800

    Opposite number

--- Fundamentals/opposite_number/src/lib.rs
+++ Fundamentals/opposite_number/src/lib.rs
@@ -11,4 +11,18 @@ mod tests {
         assert_eq!(opposite(1), -1);
         assert_eq!(opposite(-1), 1);
     }
+
+    #[test]
+    fn test_positive() {
+        for i in 0..100 {
+            assert_eq!(opposite(i), -i);
+        }
+    }
+
+    #[test]
+    fn test_negative() {
+        for i in -100..0 {
+            assert_eq!(opposite(i), -i);
+        }
+    }
 }
