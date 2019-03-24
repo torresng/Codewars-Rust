1c9a07a14148394ddf1d0001ba17659f3adcf073
Torres Ng <torresng2684cte@gmail.com>
2019-03-24 14:46:33 +0800

    DNA to RNA Conversion

--- Fundamentals/dna_to_rna_conversion/src/lib.rs
+++ Fundamentals/dna_to_rna_conversion/src/lib.rs
@@ -1,10 +1,5 @@
 pub fn dna_to_rna(dna: &str) -> String {
-    dna.chars()
-        .map(|x| match x {
-            'T' => 'U',
-            _ => x,
-        })
-        .collect()
+    dna.replace("T", "U")
 }
 
 #[cfg(test)]
